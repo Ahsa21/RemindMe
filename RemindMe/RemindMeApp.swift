dddf79a1567e18f128a41dc74d42522a15196d63
Ahsa21 <ahmadsalouka@gmail.com>
2025-12-01 06:04:08 -0800

    Initial Commit

---
+++ RemindMe/RemindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeApp.swift
+//  RemindMe
+//
+//  Created by Ahmad Saloukha on 2025-12-01.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
